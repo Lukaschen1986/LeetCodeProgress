519fc4a2727f4cccb3dadafbde4e1f5d7be9cd57
Lukas_Chen <chen_wen_dong@126.com>
2020-06-03 08:56:09 +0800

    Create 32_longest_valid_parentheses.py

---
+++ dp/32_longest_valid_parentheses.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+def longestValidParentheses(s: str) -> int:
+    if len(s) <= 1:
+        return 0
+    
+    stack = [-1]
+    res = 0
+    
+    for i in range(len(s)):
+        if s[i] == "(":
+            stack.append(i)
+        else:
+            stack.pop()
+            
+            if not stack:
+                stack.append(i)
+            
+            res = max(res, i-stack[-1])
+    
+    return res
+
+
+if __name__ == "__main__":
+    s = ")()())"
+    longestValidParentheses(s)
