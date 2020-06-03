769dd7308e52e525095539091c6606423690eade
Lukas_Chen <chen_wen_dong@126.com>
2020-06-03 20:44:38 +0800

    Create 17_letter_combinations.py

---
+++ track_back/17_letter_combinations.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+def letterCombinations(digits: str) -> List[str]:
+    phone = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+            }
+    res = []
+    
+    def track_back(combination, digits):
+        if not digits:
+            res.append(combination)
+        else:
+            for x in phone.get(digits[0]):
+                track_back(combination + x, digits[1:])
+    
+    track_back("", digits)
+    return res
+
+
+if __name__ == "__main__":
+    digits = "234"
+    letterCombinations(digits)
