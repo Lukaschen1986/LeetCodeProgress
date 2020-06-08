96e0f9a8a04c5cf85c218a9deb7fc8ba2809cba0
Lukas_Chen <chen_wen_dong@126.com>
2020-06-08 21:24:26 +0800

    Create 102_levelOrder.py

---
+++ tree/102_levelOrder.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        white, grey, depth = 0, 1, 0
+        stack = [(white, root, depth)]
+        res = []
+
+        while stack:
+            color, node, depth = stack.pop()
+
+            if not node:
+                continue
+            elif color == white:
+                stack.append((white, node.right, depth+1))
+                stack.append((white, node.left, depth+1))
+                stack.append((grey, node, depth))
+            else:
+                if len(res) == depth:
+                    res.append([])
+                res[depth].append(node.val)
+        
+        return res
