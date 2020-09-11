36e5729aade7c1005484c24e745ef6bf48246095
Lukas_Chen <chen_wen_dong@126.com>
2020-09-11 21:39:28 +0800

    Create FindClosestValueInBST.py

---
+++ AlgoExpert/easy/FindClosestValueInBST.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+class TreeNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def FindClosestValueInBST(root, target):
+    """
+    二叉查找树中最接近的值（递归）
+    O(log(n)) time | O(1) space
+    """
+    closest = float("inf")
+    
+    def helper(node, target, closest):
+        if not node:
+            return closest
+        else:
+            if abs(target - node.value) < abs(target - closest):
+                closest = node.value
+            if target > closest:
+                helper(node.right, target, closest)
+            elif target < closest:
+                helper(node.left, target, closest)
+            else:
+                return closest
+    
+    closest = helper(root, target, closest)
+    return closest
+
+
+def FindClosestValueInBST(root, target):
+    """
+    二叉查找树中最接近的值（迭代）
+    O(log(n)) time | O(1) space
+    """
+    closest = float("inf")
+    current_node = root
+    
+    while current_node:
+        if abs(target - current_node.value) < abs(target - closest):
+            closest = current_node.value
+            
+        if target > closest:
+            current_node = current_node.right
+        elif target < closest:
+            current_node = current_node.left
+        else:
+            break
+    
+    return closest
