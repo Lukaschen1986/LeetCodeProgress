c5a16fcc944839ff8ca78bff73fca80902530d74
Lukas_Chen <chen_wen_dong@126.com>
2020-10-02 16:55:59 +0800

    Update groupAnagrams.py

--- AlgoExpert/medium/groupAnagrams.py
+++ AlgoExpert/medium/groupAnagrams.py
@@ -16,6 +16,21 @@ def groupAnagrams(strs: list) -> list:
     
     return list(dct.values())
 
+
+from collections import defaultdict
+def groupAnagrams(strs: list) -> list:
+    """
+    49. 字母异位词分组
+    ​# O(w * n * log(n)) time | O(wn) space - where w is the number of words and
+    # n is the length of the longest word
+    """
+    dct = defaultdict(list)
+    
+    for word in strs:
+        k = tuple(sorted(word))
+        dct[k].append(word)
+    
+    return list(dct.values())
     
     
 if __name__ == "__main__":
