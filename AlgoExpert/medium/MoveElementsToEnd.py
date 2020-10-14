cc0a544548533f13998da9b95604a75b7100a818
Lukas_Chen <chen_wen_dong@126.com>
2020-10-14 20:50:07 +0800

    Create MoveElementsToEnd.py

---
+++ AlgoExpert/medium/MoveElementsToEnd.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+def move_elements_to_end(nums, target):
+    """
+    将指定元素移动到列表的末尾
+    # O(n) time | O(1) space
+    """
+    i = 0
+    j = len(nums) - 1
+    
+    while i < j:
+        if nums[j] == target:
+            j -= 1
+        elif (nums[j] != target) and (nums[i] == target):
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        else:
+            i += 1
+    
+    return nums
+        
+    
+if __name__ == "__main__":
+    import numpy as np
+    np.random.randint(low=0, high=10, size=10).tolist()
+    nums = [5, 0, 4, 1, 7, 5, 6, 3, 4, 8]
+    target = 5
+    move_elements_to_end(nums, target)
