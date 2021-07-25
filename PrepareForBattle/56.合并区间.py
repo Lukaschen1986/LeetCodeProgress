d6eee1b1077ad0e820b2eec07b3f121e0ec32762
Lukas_Chen <chen_wen_dong@126.com>
2021-07-25 17:29:34 +0800

    Create 56.合并区间.py

---
+++ PrepareForBattle/56.合并区间.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 17:28:36 2021
+
+@author: ASUS
+"""
+def merge(intervals: list) -> list:
+    intervals.sort()
+    res = [intervals[0]]
+
+    for pair in intervals[1:]:
+        k = res[-1][1]
+        a = pair[0]
+        b = pair[1]
+        if a > k:
+            res.append(pair)
+        elif a <= k < b:
+            previous = res.pop()
+            previous[1] = b
+            res.append(previous)
+        elif b <= k:
+            continue
+
+    return res
