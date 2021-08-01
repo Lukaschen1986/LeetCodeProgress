c26014c811b490cd4e8017c8d1a2509f1f46f2c8
Lukas_Chen <chen_wen_dong@126.com>
2021-08-01 11:33:23 +0800

    Update 56.合并区间.py

--- PrepareForBattle/56.合并区间.py
+++ PrepareForBattle/56.合并区间.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul 25 17:28:36 2021
+Created on Sun Aug  1 11:03:32 2021
 
 @author: ASUS
+
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 """
-def merge(intervals: list) -> list:
-    """
-    ​O(NlogN) time | O(N) space
-    """
+def merge(intervals):
     intervals.sort()
     res = [intervals[0]]
-
+    
     for pair in intervals[1:]:
-        k = res[-1][1]
-        a = pair[0]
-        b = pair[1]
-        if a > k:
+        last = res[-1][1]
+        start = pair[0]
+        end = pair[1]
+        if start > last:
             res.append(pair)
-        elif a <= k < b:
-            previous = res.pop()
-            previous[1] = b
-            res.append(previous)
-        elif b <= k:
-            continue
-
+        else:
+            prev = res.pop()
+            prev[1] = max(prev[1], end)
+            res.append(prev)
+    
     return res
+    
+    
+if __name__ == "__main__":
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    # [[1,6],[8,10],[15,18]]
+    merge(intervals)
+    
