fe508f42b5ea9c36b6ebca0c8e16a8b8e6db8d1c
Lukas_Chen <chen_wen_dong@126.com>
2021-08-01 16:03:28 +0800

    Create 【分治】240.搜索二维矩阵 II.py

---
+++ PrepareForBattle/【分治】240.搜索二维矩阵 II.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 15:54:42 2021
+
+@author: ASUS
+"""
+def searchMatrix(matrix, target):
+    i = 0
+    j = len(matrix[0]) - 1    
+    
+    while (i < len(matrix)) and (j >= 0):
+        if target < matrix[i][j]:
+            j -= 1
+        elif target > matrix[i][j]:
+            i += 1
+        else:
+            return True
+    
+    return False
+        
+if __name__ == "__main__":
+    matrix = [
+        [1,4,7,11,15],
+        [2,5,8,12,19],
+        [3,6,9,16,22],
+        [10,13,14,17,24],
+        [18,21,23,26,30]
+        ]
+    target = 5
+    searchMatrix(matrix, target)
