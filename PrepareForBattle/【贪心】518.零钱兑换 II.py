6c791105c7ae54bc388f9b11ce415629bae83c0f
Lukas_Chen <chen_wen_dong@126.com>
2021-08-01 17:23:41 +0800

    Create 【贪心】518.零钱兑换 II.py

---
+++ PrepareForBattle/【贪心】518.零钱兑换 II.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 17:13:51 2021
+
+@author: ASUS
+"""
+def coinChange(coins, amount):
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+    
+    for c in coins:
+        for i in range(1, len(dp)):
+            if c <= i:
+                dp[i] = dp[i] + dp[i-c]
+    
+    return dp[-1]
+    
+    
+if __name__ == "__main__":
+    coins = [11, 5, 1]
+    amount = 15
